1e083db9eea27db40eab1febd2da1f68c0b93b46
nngocjade <nngocjade@gmail.com>
2020-05-11 18:07:49 +0800

    adding login and registration files

--- src/main/java/com/muttsapp/controller/LoginController.java
+++ src/main/java/com/muttsapp/controller/LoginController.java
@@ -49,4 +49,39 @@ public class LoginController {
         throw new CustomException(" ** Cuttlefish ** ");
     }
 
+//    @RequestMapping(value="/registration", method = RequestMethod.GET)
+//    public ModelAndView registration(){
+//        ModelAndView modelAndView = new ModelAndView();
+//        User user = new User();
+//        modelAndView.addObject("user", user);
+//        modelAndView.setViewName("registration");
+//        return modelAndView;
+//    }
+//    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+//    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
+//        ModelAndView modelAndView = new ModelAndView();
+//        User userExists = userLoginService.findUserByEmail(user.getEmail());
+//        if (userExists != null) {
+//            bindingResult
+//                    .rejectValue("email", "error.user",
+//                            "There is already a user registered with the email provided");
+//        }
+//        if (bindingResult.hasErrors()) {
+//            modelAndView.setViewName("registration");
+//        } else {
+//            userLoginService.saveUser(user);
+//            modelAndView.addObject("successMessage", "User has been registered successfully");
+//            modelAndView.addObject("user", new User());
+//            modelAndView.setViewName("registration");
+//        }
+//        return modelAndView;
+//    }
+//    @RequestMapping(value="/admin/index", method = RequestMethod.GET)
+//    public String index(Authentication auth, Model model){
+//        //model.addAttribute("user_id", name);
+//        int user_id = userLoginService.findUserByEmail(auth.getName()).getUser_id();
+//        model.addAttribute("user_id", user_id);
+//        return "admin/index";
+//    }
+
 }
