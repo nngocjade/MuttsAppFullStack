396b7d71f12594e7c55f9ebe60ce6ba784b0e9cf
nngocjade <nngocjade@gmail.com>
2020-06-01 20:24:59 +0800

    dynamically added content for openNewChatModal

--- src/main/resources/static/js/muttsApp-script.js
+++ src/main/resources/static/js/muttsApp-script.js
@@ -173,7 +173,9 @@ newMessageForm.addEventListener("submit", function (event) {
 
   document.getElementById("new-message").value = " ";
 });
+
 //---------------------GET MODAL ELEMENT------------------------
+
 //---get modal element
 let modal = document.getElementById("popup-modal-window");
 
@@ -200,7 +202,82 @@ profileModalButton.addEventListener("click", openProfileModal);
 settingModalButton.addEventListener("click", openSettingsModal);
 iconProfileModalButton.addEventListener("click", openIconProfileModal);
 
+//------------CREATE SEARCH BOX---------------
+function createSearchBox() {
+  let searchFormWrap = document.createElement("div");
+  searchFormWrap.classList.add("search");
+  searchFormWrap.classList.add("form-wrap");
+  searchFormWrap.setAttribute("id", "search-form-wrap");
+
+  let searchForm = document.createElement("form");
+  searchForm.setAttribute("id", "search-contact-form");
+  searchForm.innerHTML =
+    '<i class="static - search - icon 	fa fa - search"></i>';
+
+  let searchInput = document.createElement("input");
+  searchInput.setAttribute("type", "text");
+  searchInput.setAttribute("name", "search");
+  searchInput.setAttribute("id", "search-contact-input");
+  searchInput.setAttribute("placeholder", "Search...");
+
+  searchForm.append(searchInput);
+  searchFormWrap.appendChild(searchForm);
+
+  let modalContent = document.getElementById("modal-content-id");
+  modalContent.appendChild(searchFormWrap);
+}
+
+//--------------------   CREATE CONTACT PREVIEW BOX   --------------------//
+
+function createContactPreviewBox() {
+  let contactPreviewWrapper = document.createElement("div");
+  contactPreviewWrapper.classList.add("contact-preview-wrap");
+  contactPreviewWrapper.setAttribute("id", "contact-preview-wrapper");
+
+  let contactPreviewBox = document.createElement("div");
+  contactPreviewBox.classList.add("contact-preview-box");
+
+  let imgWrap = document.createElement("div");
+  imgWrap.classList.add("img-wrap");
+  let image = document.createElement("img");
+  image.setAttribute("src", "./images/icons8-pikachu-pokemon-50.png");
+  image.setAttribute("alt", "default icon");
+
+  imgWrap.appendChild(image);
+
+  let textWrap = document.createElement("div");
+  textWrap.classList.add("contact-text-wrap");
+  let nameParagraph = document.createElement("p");
+  nameParagraph.innerText = "contact - user's - name";
+  let messageParagraph = document.createElement("p");
+  messageParagraph.innerText = "contact - last message";
+
+  contactPreviewWrapper.appendChild(contactPreviewBox);
+
+  contactPreviewBox.appendChild(imgWrap);
+  contactPreviewBox.appendChild(textWrap);
+
+  textWrap.appendChild(nameParagraph);
+  textWrap.appendChild(messageParagraph);
+
+  let modalContent = document.getElementById("modal-content-id");
+  modalContent.appendChild(contactPreviewWrapper);
+}
+
+// --------------CREATE FLOATING SEND BUTTON----------
+function createFloatingSendButton() {
+  let floatButton = document.createElement("div");
+  floatButton.classList.add("float-button");
+  floatButton.setAttribute("id", "float-button-id");
+  floatButton.innerHTML = '<i class="float-angle-icon fa fa-arrow-right"></i>';
+
+  let modalContent = document.getElementById("modal-content-id");
+  modalContent.append(floatButton);
+}
+
 function openNewChatModal() {
+  let modalContent = document.getElementById("modal-content-id");
+  modalContent.innerHTML = "";
   //need to grab elementbyid in order to set innerHTML
   let hTwoInnerText = document.getElementById("hTwo-Inner-Text");
   hTwoInnerText.innerHTML = "Select Contact";
@@ -213,13 +290,16 @@ function openNewChatModal() {
 
   modalHeaderIcon.appendChild(button);
 
+  createSearchBox();
+  createContactPreviewBox();
+  createFloatingSendButton();
   openModal();
 }
 
 function openNewGroupModal() {
   //need to grab elementbyid in order to set innerHTML
   let hTwoInnerText = document.getElementById("hTwo-Inner-Text");
-  hTwoInnerText.innerHTML = "New Group";
+  hTwoInnerText.innerHTML = "Add group participants";
 
   let modalHeaderIcon = document.getElementById("modal-header-icon");
   modalHeaderIcon.innerHTML = "";
@@ -253,6 +333,9 @@ function openIconProfileModal() {
 
   let modalHeaderIcon = document.getElementById("modal-header-icon");
   modalHeaderIcon.innerHTML = "";
+
+  let modalContent = document.getElementsByClassName("modal-content");
+  modalContent.innerHTML = "";
 
   openModal();
 }
